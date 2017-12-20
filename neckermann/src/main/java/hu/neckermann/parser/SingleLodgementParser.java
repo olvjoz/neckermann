c8c3cd1968d9ebc614783489f9ad83837875e218
Ölveti József <olveti.jozsef@neuron.hu>
2017-12-20 16:38:09 +0100

    master

--- neckermann/src/main/java/hu/neckermann/parser/SingleLodgementParser.java
+++ neckermann/src/main/java/hu/neckermann/parser/SingleLodgementParser.java
@@ -27,10 +27,6 @@ public class SingleLodgementParser {
 		
 		Elements infoTop = doc.select("div#hotel-info-top");
 		
-		Elements descs = infoTop.select("div#hotel-info-description");
-		
-		Elements infos = descs.select("div#hotel-info-card");
-		
 		for (Element element : infoTop.select("div#hotel-info-images div#slider ul.slides li img")) {
 			
 			try {
@@ -39,9 +35,27 @@ public class SingleLodgementParser {
 			} catch(Exception e) {
 				throw new IOException("Malformed document");
 			}
-			
 		}
 		
+		String uri, rating, hotelName, location, description = null;
+		Set<String> features = new HashSet<String>();
+		
+		try {
+			Elements descs = infoTop.select("div#hotel-info-description");
+			
+			Elements infos = descs.select("div#hotel-info-card");
+			
+			hotelName = infos.select("h1.hotelname").get(0).text().trim();
+			location = infos.select("h5").get(0).text().trim();
+			description = infos.select("div.product-text").get(0).text().trim();
+		} catch(Exception e) {
+			throw new IOException("Malformed document");
+		}
+		
+		lodgement.setUri(doc.baseUri());
+		lodgement.setHotelName(hotelName);
+		lodgement.setLocation(location);
+		lodgement.setDescription(description);
 		lodgement.setImages(images);
 		
 		return lodgement;
